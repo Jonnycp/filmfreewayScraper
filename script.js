87bfe36ada35f165d71887c58ff09b1957487ff5
Jonnycp <52085931+Jonnycp@users.noreply.github.com>
2024-06-25 13:59:02 +0200

    1.0 Scraper js

--- script.js
+++ script.js
@@ -3,47 +3,64 @@
 // @namespace    jonnycp
 // @version      2024-06-24
 // @description  Ottieni i dati di tutti i recensori di un festival e scaricali in csv
-// @author       Jonathan Caputo
+// @author       You
 // @match        https://filmfreeway.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=filmfreeway.com
 // @grant        none
 // ==/UserScript==
 
+const MAX_REVIEWS = 2000;
+
 const scraper = async () => {
-  console.log("SCRAPER attivo");
   let reviewSection = document.querySelector("section#reviews");
   if (reviewSection && reviewSection.dataset.reviewsSection) {
+    //INIZIO
+    console.log("SCRAPER attivo");
+    let btn = document.querySelector(
+      "section#reviews .reviews-component span a"
+    );
+    btn.innerHTML = "Caricamento...";
+
+    //ID FESTIVAL
     let reviewsLink = reviewSection.dataset.reviewsSection;
 
+    // TROVA LINK AI RECENSORI
     let nPage = 1;
     let reviewsAuthorLinks = [];
-    while (nPage <= 2000) {
-      let pageRecensori = await trovaRecensori(reviewsLink, nPage);
+    while (nPage <= MAX_REVIEWS) {
+      let pageRecensori = await findRecensori(reviewsLink, nPage);
       if (pageRecensori[0] == "EXIT") {
-        nPage = 2001;
+        nPage = MAX_REVIEWS + 1;
       } else {
         reviewsAuthorLinks.push(...pageRecensori);
         nPage += 1;
       }
     }
 
-    console.log(
-      "Trovati",
-      reviewsAuthorLinks.length,
-      "autori da",
-      window.location.href
-    );
-    console.log(reviewsAuthorLinks);
-    reviewsAuthorLinks.forEach(aut => {
-        let autObj = getInfoFromAuthor(aut);
-        if(autObj){
-          console.log(autObj)
-        }
-    })
+    //ANALISI AUTORI
+    let emoji = reviewsAuthorLinks.length >= 1 ? "😁" : "😥";
+    document.querySelector(
+      "section#reviews div.reviews-component span.active.tab"
+    ).innerHTML = reviewsAuthorLinks.length + " recensori trovati " + emoji;
+    btn.innerHTML = "Ottenendo dati...";
+
+    let autori = [];
+    reviewsAuthorLinks.forEach(async (aut) => {
+      let autObj = await getInfoFromAuthor(aut);
+      if (autObj) {
+        autori.push(autObj);
+      }
+
+      //DOWNLOAD
+      if (autori.length == reviewsAuthorLinks.length) {
+        toCSVDownload(autori);
+        btn.innerHTML = "Fatto 😎";
+      }
+    });
   }
 };
 
-const trovaRecensori = async (reviewsLink, nPage) => {
+const findRecensori = async (reviewsLink, nPage) => {
   const d = await fetch(reviewsLink + "?page=" + nPage);
   const d_1 = await d.json();
   const d_2 = document.createRange().createContextualFragment(d_1.success);
@@ -66,47 +83,107 @@ const trovaRecensori = async (reviewsLink, nPage) => {
 
 const getInfoFromAuthor = async (linkAuthor) => {
   const d = await fetch(linkAuthor);
-    const d_1 = await d.text();
-    const d_2 = document.createRange().createContextualFragment(d_1);
-    let nome = d_2.querySelector("div.Header-profileName")
-        ? d_2.querySelector("div.Header-profileName").innerHTML
-        : "";
-    let title = d_2.querySelector("div.Header-profileTitle")
-        ? d_2.querySelector("div.Header-profileTitle").innerHTML
-        : "";
-    let bio = d_2.querySelector("div.Header-Profile-introCopy")
-        ? d_2.querySelector("div.Header-Profile-introCopy").innerHTML
-        : "";
-    let email = d_2.querySelector("meta[itemprop='email']")
-        ? d_2.querySelector("meta[itemprop='email']").content
-        : "";
-    let bd = d_2.querySelector("div[itemprop='birthDate']")
-        ? d_2.querySelector("div[itemprop='birthDate']").content
-        : "";
-    let city = "";
-    let labels = ["Current City", "Hometown", "Birth City"];
-    for (let label of labels) {
-        let labelElement = Array.from(
-            document.querySelectorAll(".Grid .TextStyle-medium")
-        ).find((el) => el.textContent === label);
-        let cityElement = labelElement
-            ? labelElement.parentElement.nextElementSibling
-            : null;
-        city = cityElement ? cityElement.textContent : null;
+  const d_1 = await d.text();
+  const d_2 = document.createRange().createContextualFragment(d_1);
+  let nome = d_2.querySelector("div.Header-profileName")
+    ? d_2.querySelector("div.Header-profileName").innerHTML
+    : "";
+  let title = d_2.querySelector("div.Header-profileTitle")
+    ? d_2.querySelector("div.Header-profileTitle").innerHTML
+    : "";
+  let bio = d_2.querySelector("div.Header-Profile-introCopy")
+    ? d_2.querySelector("div.Header-Profile-introCopy").innerHTML
+    : "";
+  let email = d_2.querySelector("meta[itemprop='email']")
+    ? d_2.querySelector("meta[itemprop='email']").content
+    : "";
+  let bd = d_2.querySelector("div[itemprop='birthDate']")
+    ? d_2.querySelector("div[itemprop='birthDate']").content
+    : "";
+  let city = "";
+  let labels = ["Current City", "Hometown", "Birth City"];
+  for (let label of labels) {
+    let labelElement = Array.from(
+      document.querySelectorAll(".Grid .TextStyle-medium")
+    ).find((el) => el.textContent === label);
+    let cityElement = labelElement
+      ? labelElement.parentElement.nextElementSibling
+      : null;
+    city = cityElement ? cityElement.textContent : null;
 
-        if (city) break;
-    }
-    return {
-        "name": nome.split(" ").slice(0, -1).join(" "),
-        "lastName": nome.split(" ").slice(-1),
-        "title": title,
-        "bio": bio,
-        "email": email,
-        "birthday": bd,
-        "city": city,
-        "author": autoreLink,
-        "festival": window.location.href
-    };
+    if (city) break;
+  }
+  return {
+    name: nome.split(" ").slice(0, -1).join(" "),
+    lastName: nome.split(" ").slice(-1).join(" "),
+    email: email,
+    title: title,
+    city: city,
+    bio: bio,
+    birthday: bd,
+    author: linkAuthor,
+    festival: window.location.href,
+  };
 };
 
-document.addEventListener("readystatechange", () => setTimeout(scraper, 1000));
+const addButtonScraper = () => {
+  let container = document.querySelector(
+    "section#reviews .reviews-component span"
+  );
+  let span = document.createElement("span");
+  let btn = document.createElement("a");
+  let classes = [
+    "btn",
+    "btn-h4",
+    "btn-white",
+    "header-action",
+    "pull-right",
+    "hidden-xs",
+    "ProfileFestival-ratingsInHeaderButton",
+  ];
+  classes.forEach((c) => btn.classList.add(c));
+  btn.innerText = "Scarica autori";
+  btn.addEventListener("click", scraper);
+  span.appendChild(btn);
+  if (container) {
+    container.insertAdjacentElement("afterend", span);
+    container.remove();
+  }
+};
+
+const toCSVDownload = (data) => {
+  let csvContent = "";
+  let header = [
+    "Nome",
+    "Cognome",
+    "email",
+    "Titolo",
+    "Citta",
+    "Bio",
+    "Compleanno",
+    "Link",
+    "Festival",
+  ];
+  csvContent += header.join(",") + "\n";
+
+  console.log(data);
+  for (let row of data) {
+    let values = Object.values(row);
+    csvContent += values.join(",") + "\n";
+  }
+
+  let blob = new Blob([csvContent], { type: "text/csv" });
+  let url = URL.createObjectURL(blob);
+
+  let a = document.createElement("a");
+  a.href = url;
+  let festival = window.location.href.split("/");
+  a.download = "Recensori festival " + festival[festival.length - 1] + ".csv";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  URL.revokeObjectURL(url);
+};
+
+document.addEventListener("readystatechange", addButtonScraper);
